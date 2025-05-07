5be2e80ff67e283a791c10b66a515fe588eb9722
Ignacio <73908290+Nkiex@users.noreply.github.com>
2025-05-07 14:14:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
-
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/contadorDeClicks/', 
+  base: '/contadorDeClicks/', // 👈 Muy importante para GitHub Pages
   plugins: [react()],
 })
