abe67c47c137a0bff3e0443dd258d9aef6cbbd90
Ignacio <73908290+Nkiex@users.noreply.github.com>
2025-05-07 14:12:03 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/contadorDeClicks/', 
+  plugins: [react()],
+})
